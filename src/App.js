6ffdc349a1bde3be1ed6e407e9e4ed009d569fed
Erick Katzenstein <erick.katzenstein@gmail.com>
2016-12-16 17:49:16 -0500

    codemirror init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CodeMirror from 'codemirror';
+class App extends Component {
+  render() {
+    {console.log(CodeMirror)}
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
